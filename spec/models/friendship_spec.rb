5c98cff3a09f020cda93942647ed49e8c2f02111
ngani faith <nganifaith@gmail.com>
2021-04-08 11:14:30 +0100

    Add model test for Post, commet, likes and friendship

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user)}
+  end
+  describe 'associations' do
+    it { should belong_to(:friend)}
+  end
 end
