e04bb2a31334a97e3ebde4a76c3d41e84d46215d
ngani faith <nganifaith@gmail.com>
2021-04-08 09:58:07 +0100

    Add pending friends and friend_request

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,15 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foriegn_key: 'friend_id'
+
+  # Users who have yet to confirme friend requests
+
+  def pending_friends
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+  end
 end
